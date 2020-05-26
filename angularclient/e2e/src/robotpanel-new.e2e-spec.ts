950610cbe94b27dba8e535e33d81b86fb969ef94
znowack <z.nowacki@o2.pl>
2020-05-26 22:11:09 +0200

    deleted example test

--- angularclient/e2e/src/robotpanel-new.e2e-spec.ts
+++ angularclient/e2e/src/robotpanel-new.e2e-spec.ts
@@ -1,23 +1,6 @@
 import { AppPage } from './app.po';
-import { browser, logging, by, element } from 'protractor';
-
-describe('angularjs homepage todo list', function () {
-    it('should add a todo', function () {
-        browser.get('https://angularjs.org');
-
-        element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-        element(by.css('[value="add"]')).click();
-
-        var todoList = element.all(by.repeater('todo in todoList.todos'));
-        expect(todoList.count()).toEqual(3);
-        expect(todoList.get(2).getText()).toEqual('write first protractor test');
-
-        // You wrote your first test, cross it off the list
-        todoList.get(2).element(by.css('input')).click();
-        var completedAmount = element.all(by.css('.done-true'));
-        expect(completedAmount.count()).toEqual(2);
-    });
-});
+import { browser, by, element } from 'protractor';
+import { protractor } from 'protractor/built/ptor';
 
 describe('robot status', function () {
     let page: AppPage;
@@ -31,6 +14,10 @@ describe('robot status', function () {
 
         expect(
             element.all(by.cssContainingText('h3', 'Status robota')).count()
+        ).toEqual(1);
+
+        expect(
+            element.all(by.cssContainingText('button', 'refresh')).count()
         ).toEqual(1);
 
         expect(
@@ -52,5 +39,11 @@ describe('robot status', function () {
         expect(
             element.all(by.cssContainingText('th', 'Bateria')).count()
         ).toEqual(1);
+    });
+
+    it('should refresh robot status table', () => {
+        page.navigateToLocation('robotpanel-new/refreshtest');
+
+        element(by.cssContainingText('button', 'refresh')).click();
     });
 });
